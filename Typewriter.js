3297dee008362504eb44e69642342f23cbac154f
irenenyatichi <irenenyatichik@gmail.com>
2021-05-12 17:08:50 +0300

    first commit

---
+++ Typewriter.js
@@ -0,0 +1,28 @@
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text").Index.HTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+const textDisplay = document.getElementById('text')
+const phrases = ["... Irene Nyatichi  ..."]
+let i = 0
+let j =0
+let currentPhrase = []
+
+function loop(){
+  textDisplay.innerHTML = currentPhrase.join('')
+  if (i<phrases.length){
+    console.log(phrases[i])//this is the outer loop
+
+    if (j <=phrases[i].length){
+      currentPhrase.push(phrases[i][j])
+      j++
+      console.log("add a letter" , j)
+    }
+  setTimeout(loop, 120)
+}
+}
+loop();
+
