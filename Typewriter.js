fcc996323cf708b56d2d0b30ffbedbc966024d21
irenenyatichi <79199068+irenenyatichi@users.noreply.github.com>
2021-05-18 22:50:19 +0300

    Update Typewriter.js

--- Typewriter.js
+++ Typewriter.js
@@ -6,7 +6,7 @@ function typeWriter() {
   }
 }
 const textDisplay = document.getElementById('text')
-const phrases = ["... Irene Nyatichi , Welcome ..."]
+const phrases = ["... Irene Nyatichi  ,  Welcome  ..."]
 let i = 0
 let j =0
 let currentPhrase = []
